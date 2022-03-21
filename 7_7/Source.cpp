65d3ad68e671af876c22e61da9c7bcd29dc4c96b
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-03-22 00:57:35 +0700

    last version

--- 7_7/Source.cpp
+++ 7_7/Source.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void findANDreplace(char* str, char* resault);
-void convert(float y, char* str);
+void convert(float y, float z, char* str);
 
 int main()
 {
@@ -17,28 +17,29 @@ int main()
 	cin >> y;
 	cout << "Введите z: ";
 	cin >> z;
-	char res[50];
-	convert(y, res);
+	char res[100];
+	convert(y, z, res);
 	cout << res << endl;
 	char* str1 = new char[100];
 	char* str2 = new char[110];
-	strcpy(str1, "Artem KARAsAA@WA");
+	strcpy(str1, "OLEG BALDA");
 	findANDreplace(str1, str2);
 	cout << str1 << endl;
 	cout << str2 << endl;
 	delete[]str1;
 	delete[]str2;
 }
-void convert(float y, char* str)
+void convert(float y, float z, char* str)
 {
-	double x = sin(3 * y) + 16.5;
-	sprintf(str, "%08.8f = sin(3* %08.8f)+16.5", x, y);
+	double x = y+3*exp(z);
+	sprintf(str, "%05.5f = %05.5f + 3*exp(%05.5f)", x, y, z);
 }
 
 void findANDreplace(char* str_l, char* resault_l) {
 	memset(resault_l, 0, sizeof(char) * 100);
 
 	char exchange[] = " A-A-A ";
+	int counter = 0;
 
 	char str[250] = {};
 	for (short i = 0; i < strlen(str_l); i++) {
@@ -60,45 +61,26 @@ void findANDreplace(char* str_l, char* resault_l) {
 	}
 
 	memset(str, 0, sizeof(char) * 250);
-	char res[250] = {};
+
 	int iii = 0;
 	char str_t[100] = {};
 	char str_t2[100]  = {};
 	int pass = 0;
+	int sz = 0;
+
 	for ( ;i < strlen(str1); ) {
 		strncpy(str, i + str1, 50);
 		iii = strcspn(str, "A");
 		strncpy(str_t, i + str, iii);
-		i = i + iii+1;
-		cout << str1[i]<<endl;
-	}
-
-	/*for (; i < ii - 1;) {
-		strncpy(str1, i + str, 50);
-		iii = strcspn(str1, " ");
-
 		i = i + iii + 1;
-		if (static_cast<int>('А') <= static_cast<int>(str[i]) && static_cast<int>('Я') >= static_cast<int>(str[i])) {
-			if (i > ii) break;
-			addres = i;
+		strncpy(sz+str_t2, str, iii);
+		if (str1[i - 1] == 'A') {
+			strcat(str_t2, exchange);
+			counter++;
 		}
+		sz = strlen(str_t2);
 	}
-
-	char str2[250] = {};
-	strncpy(str2, (addres)+str, 50);
-	short iii_p = strcspn(str2, " ");
-	short b = addres + iii_p;
-	short a = addres;
-
-	char end_resault[250] = {};
-	char addstr[] = { "Кукушечка" };
-	strncpy(end_resault, str, a);
-	strcat(end_resault, addstr);
-	strncpy(str1, b + str, 50);
-	strcat(end_resault, str1);
-
-	for (int i = 0; i < strlen(end_resault); i++)
-	{
-		resault_l[i] = end_resault[i];
-	}*/
+	for (int i = 0; i < strlen(str_t2); i++) {
+		resault_l[i] = str_t2[i];
+	}
 }
